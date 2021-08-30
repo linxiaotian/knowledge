bdef3a48e8f50e729770de08ad5b6572090df0bb
林强 <19817153610@139.com>
2021-08-30 19:18:03 +0800

    week04

---
+++ src/main/java/week04/test/ThreadA.java
@@ -0,0 +1,15 @@
+package week04.test;
+
+public class ThreadA extends Thread {
+
+    @Override
+    public void run() {
+        super.run();
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("这是线程A");
+    }
+}
